13f6023fd7d3696b268d2bfccaf8df7e50b668b2
Alex Gleason <alex@alexgleason.me>
2024-04-12 22:50:40 -0500

    Add webhook route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,31 @@
 import { Hono } from 'hono';
+import { nip19 } from 'nostr-tools';
 
 const app = new Hono();
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
+});
+
+app.post('/:bech32', async (c) => {
+  const bech32 = c.req.param('bech32');
+  const data = nip19.decode(bech32);
+
+  const json = await c.req.json();
+  console.log(json);
+
+  switch (data.type) {
+    case 'npub':
+      // TODO: Remote sign
+      console.log('npub', data);
+      break;
+    case 'nsec':
+      // TODO: Local sign
+      console.log('nsec', data);
+      break;
+  }
+
+  return c.text('');
 });
 
 Deno.serve(app.fetch);
